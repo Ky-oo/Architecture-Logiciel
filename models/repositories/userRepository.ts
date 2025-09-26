c36ef3288571ad74315744fec07cfe4dafd60724
Ky-oo <kylian.patry@gmail.com>
2025-09-26 09:08:00 +0200

    Create IUserRepository + Create class for userService and userRepository

--- models/repositories/userRepository.ts
+++ models/repositories/userRepository.ts
@@ -4,55 +4,52 @@ import type {
   IUserCreate,
   IUserUpdate,
 } from "../interfaces/User.interfaces.ts";
+import { IUserRepository } from "../../Interfaces/IUserRepository.ts";
 
-const getUsers = async (): Promise<IUser[]> => {
-  const users = await User.findAll();
-  return users.map((user) => user.get() as IUser);
-};
+class UserRepository implements IUserRepository {
+  async getUsers(): Promise<IUser[]> {
+    const users = await User.findAll();
+    return users.map((user) => user.get() as IUser);
+  }
 
-const getUserByPk = async (userId: string): Promise<IUser | null> => {
-  const user = await User.findByPk(userId);
-  if (!user) return null;
-  return user.get() as IUser;
-};
+  async getUserByPk(userId: string): Promise<IUser | null> {
+    const user = await User.findByPk(userId);
+    if (!user) return null;
+    return user.get() as IUser;
+  }
 
-const createUser = async (body: IUserCreate): Promise<IUser> => {
-  const { firstname, lastname, email, phoneNumber } = body;
-  const newUser = await User.create({
-    firstname,
-    lastname,
-    email,
-    phoneNumber,
-    role: email.split("@")[1] === "company.com" ? "Admin" : "User",
-  });
-  return newUser.get() as IUser;
-};
+  async createUser(body: IUserCreate): Promise<IUser> {
+    const { firstname, lastname, email, phoneNumber } = body;
+    const newUser = await User.create({
+      firstname,
+      lastname,
+      email,
+      phoneNumber,
+      role: email.split("@")[1] === "company.com" ? "Admin" : "User",
+    });
+    return newUser.get() as IUser;
+  }
 
-const updateUser = async (
-  body: IUserUpdate,
-  userId: string
-): Promise<IUser> => {
-  const { firstname, lastname, email, phoneNumber, role } = body;
-  const user = await User.findByPk(userId);
-  if (!user) throw new Error("User not found");
+  async updateUser(body: IUserUpdate, userId: string): Promise<IUser> {
+    const { firstname, lastname, email, phoneNumber, role } = body;
+    const user = await User.findByPk(userId);
+    if (!user) throw new Error("User not found");
+    user.set({
+      firstname: firstname || user.get("firstname"),
+      lastname: lastname || user.get("lastname"),
+      email: email || user.get("email"),
+      phoneNumber: phoneNumber || user.get("phoneNumber"),
+      role: role || user.get("role"),
+    });
+    await user.save();
+    return user.get() as IUser;
+  }
 
-  user.set({
-    firstname: firstname || user.get("firstname"),
-    lastname: lastname || user.get("lastname"),
-    email: email || user.get("email"),
-    phoneNumber: phoneNumber || user.get("phoneNumber"),
-    role: role || user.get("role"),
-  });
+  async deleteUser(userId: string): Promise<void> {
+    const user = await User.findByPk(userId);
+    if (!user) throw new Error("User not found");
+    await user.destroy();
+  }
+}
 
-  await user.save();
-
-  return user.get() as IUser;
-};
-
-const deleteUser = async (userId: string): Promise<void> => {
-  const user = await User.findByPk(userId);
-  if (!user) throw new Error("User not found");
-  await user.destroy();
-};
-
-export default { getUsers, getUserByPk, createUser, updateUser, deleteUser };
+export default new UserRepository();
--- services/userService.ts
+++ services/userService.ts
@@ -6,10 +6,11 @@ import type {
 } from "../models/interfaces/User.interfaces.ts";
 import { IUserService } from "../Interfaces/IUserService.ts";
 
-const userService: IUserService = {
+class UserService implements IUserService {
   async getUsers(): Promise<IUser[]> {
     return await UserRepository.getUsers();
-  },
+  }
+
   async createUser(body: IUserCreate): Promise<IUser> {
     const { firstname, lastname, email, phoneNumber } = body;
     const newUser = await UserRepository.createUser({
@@ -19,14 +20,16 @@ const userService: IUserService = {
       phoneNumber,
     });
     return newUser;
-  },
+  }
+
   async updateUser(body: IUserUpdate, userId: string): Promise<IUser> {
     const user = await UserRepository.updateUser(body, userId);
     return user;
-  },
+  }
+
   async deleteUser(userId: string): Promise<void> {
     await UserRepository.deleteUser(userId);
-  },
-};
+  }
+}
 
-export default userService;
+export default new UserService();
