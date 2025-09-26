99fef6e6f3623439ffc5d7e2a9a39c4c840c01d6
Ky-oo <kylian.patry@gmail.com>
2025-09-26 09:16:05 +0200

    Use classes

--- Présentation/routes/users.ts
+++ Présentation/routes/users.ts
@@ -1,26 +1,26 @@
 import express from "express";
+import UserService from "../../services/userService";
 const router = express.Router();
-import userService from "../../services/userService";
 
 router.get("/", async function (req: any, res: any, next: any) {
-  const users = await userService.getUsers();
+  const users = await UserService.getUsers();
   res.status(200).json({ users });
 });
 
 router.post("/", async function (req: any, res: any, next: any) {
-  const newUser = await userService.createUser(req.body);
+  const newUser = await UserService.createUser(req.body);
   res.status(201).json({ newUser });
 });
 
 router.put("/:id", async function (req: any, res: any, next: any) {
   const { id } = req.params;
-  const user = await userService.updateUser(req.body, id);
+  const user = await UserService.updateUser(req.body, id);
   res.status(200).json({ user });
 });
 
 router.delete("/:id", async function (req: any, res: any, next: any) {
   const { id } = req.params;
-  await userService.deleteUser(id);
+  await UserService.deleteUser(id);
   res.status(204).end();
 });
 
